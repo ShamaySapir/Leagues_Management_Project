18a3b1dd6e247c77eee7a58231a2eeb0cdb0d84c
barif <barifergan10@gmail.com>
2021-06-05 15:54:13 +0300

    create files

---
+++ src/main.js
@@ -0,0 +1,88 @@
+//#region global imports
+const DButils = require("./dataLayer/utils/DButils");
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
+//#endregion
+//#region express configures
+var express = require("express");
+var path = require("path");
+const session = require("client-sessions");
+var logger = require("morgan");
+var cors = require("cors");
+var app = express();
+
+app.use(logger("dev")); //logger
+app.use(express.json()); // parse application/json
+app.use(
+    session({
+        cookieName: "session", // the cookie key name
+        secret: process.env.COOKIE_SECRET, // the encryption key
+        duration: 24 * 60 * 60 * 1000, // expired after 24 hours
+        activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration,
+        cookie: {
+            httpOnly: false,
+        },
+        lastSearch: null,
+        lastResponse: null,
+    })
+);
+app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname, "public"))); //To serve static files such as images, CSS files, and JavaScript files
+
+// middleware to serve all the needed static files under the dist directory - loaded from the index.html file
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("dist"));
+
+const corsConfig = {
+    origin: true,
+    credentials: true,
+};
+
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
+
+const port = process.env.PORT;
+
+const auth = require("./serviceLayer/routes/auth");
+const matches = require("./serviceLayer/routes/matches");
+const referees = require("./serviceLayer/routes/referees");
+const unionRep = require("./serviceLayer/routes/unionRep");
+
+
+//#endregion
+
+//#region cookie middleware
+app.use(function(req, res, next) {
+    if (req.session && req.session.user_id) {
+        DButils.execQuery("SELECT userId FROM users")
+            .then((users) => {
+                if (users.find((x) => x.user_id === req.session.user_id)) {
+                    req.user_id = req.session.user_id;
+                }
+                next();
+            })
+            .catch((error) => next());
+    } else {
+        next();
+    }
+});
+//#endregion
+
+// ----> For cheking that our server is alive
+app.get("/alive", (req, res) => res.send("I'm alive"));
+// Routings
+app.use("/matches", matches)
+app.use("/referees", referees);
+app.use("/unionRep", unionRep);
+
+app.use(auth);
+
+app.use(function(err, req, res, next) {
+    console.error(err);
+    res.status(err.status || 500).send(err.message);
+});
+
+const server = app.listen(port, () => {
+    console.log(`Server listen on port ${port}`);
+});
---
+++ src/serviceLayer/routes/auth.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+const DButils = require("./dataLayer/utils/DButils");
+const bcrypt = require("bcryptjs");
+
+
+router.post("/Login", async(req, res, next) => { //check if login is post request or get
+    try {
+        const user = (
+            await DButils.execQuery(
+                `SELECT * FROM dbo.users WHERE username = '${req.body.username}'`
+            )
+        )[0];
+        // check that username exists & the password is correct
+        if (!user || !bcrypt.compareSync(req.body.password, user.passwordHash)) {
+            throw { status: 401, message: "Username or Password incorrect" };
+        }
+        // Set cookie
+        req.session.user_id = user.userId;
+        // return cookie
+        res.status(200).send("Login successfully");
+    } catch (error) {
+        next(error);
+    }
+});
