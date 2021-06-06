40c77a9ac4c00bb4f050fcb6f9f4d9d7e11ce1c5
ostrovamit <82804444+ostrovamit@users.noreply.github.com>
2021-06-06 18:27:36 +0300

    policy 2

--- src/domainLayer/unionRep_domain.js
+++ src/domainLayer/unionRep_domain.js
@@ -2,118 +2,178 @@ const axios = require("axios");
 const matches_utils = require("../dataLayer/utils/matches_utils");
 const api_domain = "https://soccer.sportmonks.com/api/v2.0";
 
-
 //policy == 1 : schedule matches so every two teams in the leage play together once
 async function scheduleByFirstPolicy(leagueId, seasonId) {
-    let teams = await axios.get(`${api_domain}/teams/season/${seasonId}`, {
-        params: {
-            include: "league",
-            api_token: process.env.api_token,
-        },
-    });
-    teams = getTeamsByLeagueId(teams, leagueId);
+  let teams = await axios.get(`${api_domain}/teams/season/${seasonId}`, {
+    params: {
+      include: "league",
+      api_token: process.env.api_token,
+    },
+  });
+  teams = getTeamsByLeagueId(teams, leagueId);
+  console.log(teams.data.data);
+  console.log("---------------------");
 
-    let season = await axios.get(`${api_domain}/seasons/${seasonId}`, {
-        params: {
-            api_token: process.env.api_token,
-        },
-    });
+  let season = await axios.get(`${api_domain}/seasons/${seasonId}`, {
+    params: {
+      api_token: process.env.api_token,
+    },
+  });
 
-    let matches = [];
-    //TODO: check how the teams look like 
-    let season_name = season.data.data.name;
-    let years = season_name.split('/');
-    let year = years[0];
-    var date = year + "-01-01";
-    date = new Date(date).toDateString();
+  let matches = [];
+  //TODO: check how the teams look like
+  let season_name = season.data.data.name;
+  let years = season_name.split("/");
+  let year = years[0];
+  var date = year + "-01-01";
+  date = new Date(date).toDateString();
 
-    let match_id = await getcurrentMatchId();
+  let match_id = await getcurrentMatchId();
 
-    for (let index = 0; index < Object.size(teams); index++) {
-        for (let j = index + 1; j < Object.size(teams); j++) {
-            let stage_id = await getStageBySeasonId(seasonId);
-            let stadium = await getStadium(teams.data.data[index].id);
-            let match = {
-                matchId: match_id + 1,
-                leagueId: leagueId,
-                seasonId: seasonId,
-                stageId: stage_id,
-                matchDate: date,
-                matchHour: "20:00:00",
-                homeTeam: teams.data.data[index],
-                awayTeam: teams.data.data[j],
-                stadium: stadium,
-                refereeId: null,
-                score: null,
-            };
+  for (let index = 0; index < teams.data.data.length; index++) {
+    for (let j = index + 1; j < teams.data.data.length; j++) {
+      let stage_id = await getStageBySeasonId(seasonId);
+      let stadium = await getStadium(teams.data.data[index].id);
+      let match = {
+        matchId: match_id + 1,
+        leagueId: leagueId,
+        seasonId: seasonId,
+        stageId: stage_id,
+        matchDate: date,
+        matchHour: "20:00:00",
+        homeTeam: teams.data.data[index],
+        awayTeam: teams.data.data[j],
+        stadium: stadium,
+        refereeId: null,
+        score: null,
+      };
 
-            matches.push(match);
-            date = addDays(date, 7);
-            match_id++;
-        }
+      matches.push(match);
+      date = addDays(date, 7);
+      match_id++;
     }
+  }
+  console.log(matches);
 
-    //TODO: add matches to DB
-
+  //TODO: add matches to DB
 }
 
 async function scheduleBySecondPolicy(leagueId, seasonId) {
+  console.log("policy 2!!!!!!!!!!!!!");
+  let teams = await axios.get(`${api_domain}/teams/season/${seasonId}`, {
+    params: {
+      include: "league",
+      api_token: process.env.api_token,
+    },
+  });
+  teams = getTeamsByLeagueId(teams, leagueId);
+  //   console.log(teams.data.data);
+  //   console.log("---------------------");
 
+  let season = await axios.get(`${api_domain}/seasons/${seasonId}`, {
+    params: {
+      api_token: process.env.api_token,
+    },
+  });
+
+  let firstRoundMatches = [];
+  //TODO: check how the teams look like
+  let season_name = season.data.data.name;
+  let years = season_name.split("/");
+  let year = years[0];
+  var date = year + "-01-01";
+  date = new Date(date).toDateString();
+
+  let match_id = await getcurrentMatchId();
+
+  for (let index = 0; index < teams.data.data.length; index++) {
+    for (let j = index + 1; j < teams.data.data.length; j++) {
+      let stage_id = await getStageBySeasonId(seasonId);
+      let stadium = await getStadium(teams.data.data[index].id);
+      let match = {
+        matchId: match_id + 1,
+        leagueId: leagueId,
+        seasonId: seasonId,
+        stageId: stage_id,
+        matchDate: date,
+        matchHour: "20:00:00",
+        homeTeam: teams.data.data[index],
+        awayTeam: teams.data.data[j],
+        stadium: stadium,
+        refereeId: null,
+        score: null,
+      };
+
+      firstRoundMatches.push(match);
+      date = addDays(date, 7);
+      match_id++;
+    }
+  }
+  secondRoundMatches = [...firstRoundMatches];
+  //switch between homeTeam and awayTeam fot the second round
+  secondRoundMatches.forEach((match) => {
+    let former_homeTeam = match.homeTeam;
+    let former_awayTeam = match.awayTeam;
+    match.homeTeam = former_awayTeam;
+    match.awayTeam = former_homeTeam;
+  });
+
+  allMatches = firstRoundMatches.concat(secondRoundMatches);
+  console.log(allMatches);
+
+  //TODO: add matches to DB
 }
 
 function getTeamsByLeagueId(teams_info, leagueId) {
-    let index = 0;
-    teams_info.data.data.map((team_info) => {
-        const { league } = team_info;
-        const { id } = league.data;
+  let index = 0;
+  teams_info.data.data.map((team_info) => {
+    const { league } = team_info;
+    const { id } = league.data;
 
-        if (id != leagueId) {
-            teams_info.data.data.slice(index);
-        }
-        index++;
-    });
-    return teams_info;
+    if (id != leagueId) {
+      teams_info.data.data.slice(index);
+    }
+    index++;
+  });
+  return teams_info;
 }
 
 async function getStageBySeasonId(seasonId) {
-    let stage = await axios.get(`${api_domain}/stages/season/${seasonId}`, {
-        params: {
-            api_token: process.env.api_token,
-        },
-    });
+  let stage = await axios.get(`${api_domain}/stages/season/${seasonId}`, {
+    params: {
+      api_token: process.env.api_token,
+    },
+  });
 
-    let size = stage.data.data.length;
-    let index = Math.floor(Math.random() * size);
+  let size = stage.data.data.length;
+  let index = Math.floor(Math.random() * size);
 
-    let curr_stage = stage.data.data[index].id;
-    return curr_stage;
-
+  let curr_stage = stage.data.data[index].id;
+  return curr_stage;
 }
 
 function addDays(date, days) {
-    var result = new Date(date);
-    result.setDate(result.getDate() + days);
-    result = result.toDateString();
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  result = result.toDateString();
 
-    return result;
+  return result;
 }
 
 async function getStadium(team_id) {
+  let team = await axios.get(`${api_domain}/teams/${team_id}`, {
+    params: {
+      include: "venue",
+      api_token: process.env.api_token,
+    },
+  });
 
-    let team = await axios.get(`${api_domain}/teams/${team_id}`, {
-        params: {
-            include: "venue",
-            api_token: process.env.api_token,
-        },
-    });
-
-    return team.data.data.venue.data.name;
+  return team.data.data.venue.data.name;
 }
 
-
 async function getcurrentMatchId() {
-    let table_size = matches_utils.getTableSize();
-    return table_size;
+  let table_size = matches_utils.getTableSize();
+  return table_size;
 }
 
 exports.scheduleByFirstPolicy = scheduleByFirstPolicy;
--- src/serviceLayer/routes/auth.js
+++ src/serviceLayer/routes/auth.js
@@ -1,27 +1,27 @@
 var express = require("express");
 var router = express.Router();
-const path = require('path');
-const DButils = require(path.join(process.cwd(), "./src/dataLayer/utils/DButils"));
+const path = require("path");
+const DButils = require("../../dataLayer/utils/DButils");
 const bcrypt = require("bcryptjs");
 
-
-router.post("/Login", async(req, res, next) => { //check if login is post request or get
-    try {
-        const user = (
-            await DButils.execQuery(
-                `SELECT * FROM dbo.users WHERE username = '${req.body.username}'`
-            )
-        )[0];
-        // check that username exists & the password is correct
-        if (!user || !bcrypt.compareSync(req.body.password, user.passwordHash)) {
-            throw { status: 401, message: "Username or Password incorrect" };
-        }
-        // Set cookie
-        req.session.user_id = user.userId;
-        // return cookie
-        res.status(200).send("Login successfully");
-    } catch (error) {
-        next(error);
+router.post("/Login", async (req, res, next) => {
+  //check if login is post request or get
+  try {
+    const user = (
+      await DButils.execQuery(
+        `SELECT * FROM dbo.users WHERE username = '${req.body.username}'`
+      )
+    )[0];
+    // check that username exists & the password is correct
+    if (!user || !bcrypt.compareSync(req.body.password, user.passwordHash)) {
+      throw { status: 401, message: "Username or Password incorrect" };
     }
+    // Set cookie
+    req.session.user_id = user.userId;
+    // return cookie
+    res.status(200).send("Login successfully");
+  } catch (error) {
+    next(error);
+  }
 });
 module.exports = router;
