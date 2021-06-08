21ea6b53e268e0886875e90f98b64f099d93da02
galbane-code <62691019+galbane-code@users.noreply.github.com>
2021-06-08 12:37:51 +0300

    change is referre domain

--- src/domainLayer/referees_domain.js
+++ src/domainLayer/referees_domain.js
@@ -1,64 +1,87 @@
 const axios = require("axios");
-const { NText } = require("mssql");
+
 const referee_utils = require("../dataLayer/utils/referee_utils");
-const api_domain = "https://soccer.sportmonks.com/api/v2.0";
 const auth_domain = require("../domainLayer/auth_domain");
-const auth_utils = require("../dataLayer/utils/auth_utils");
 const matches_utils = require("../dataLayer/utils/matches_utils");
-const DButils = require("../dataLayer/utils/DButils.js");
+const DButils = require("./dataLayer/utils/DButils.js");
 
-
-async function CreateReferee(req) {
-    let userName = req.body.username;
-    let userFirstName = req.body.firstName;
-    let userLastName = req.body.lastName;
+async function CreateRefree(req) 
+{
+    let userName = req.body.userName;
+    let userFirstName = req.body.userFirstname;
+    let userLastName = req.body.userLastname;
     let country = req.body.country;
     let password = req.body.password;
     let email = req.body.email;
     let image;
+  
 
-    let user = {
-        username: userName,
-        firstName: userFirstName,
-        lastName: userLastName,
-        country: country,
-        password: password,
-        email: email,
+    let user = 
+    {
+        username : userName,
+        firstName : userFirstName,
+        lastName :userLastName,
+        country : country,
+        password : password,
+        email : email,
     }
-
-    if (req.body.image) {
+    
+    if(req.body.image)
+    {
         image = req.body.image;
         user.image = image;
     }
 
-    try {
+
+    try
+    {
         user = await auth_domain.userRegister(user);
-    } catch (error) {
-        console.log(error);
+    }
+    catch (error)
+    {
+        throw { error: error}
     }
 
-    try {
-        await insertToRefereeTable(user);
-    } catch (error) {
-        throw { error: error }
-    }
-
-    try {
+    try
+    {
         //await insertToRefreeTable(user);
         await RegisterArefereeToMatch();
-    } catch (error) {
-        throw { error: error }
     }
+    catch (error)
+    {
+        throw { error: error}
+    }
+
+
 }
 
-async function RegisterArefereeToMatch() {
+
+async function insertToRefreeTable(user)
+{
+    await referee_utils.insertRefereeInfo(user.userId,user.username)
+} 
+
+async function RegisterArefereeToMatch()
+{
     //let matches = await matches_utils.getMatches();
-    let matches = await DButils.getTableSize('Users');
-    let user = 4;
-}
+    let matchesLength = await DButils.getTableSize('matches');
+    let refereeLength = await DButils.getTableSize('Referees');
 
-async function insertToRefereeTable(user) {
-    await referee_utils.insertRefereeInfo(user);
-}
+    // in case the referee is less then then the games
+    if(refereeLength <= matchesLength)
+    {
+        matches_utils.UpdateRefereeToMatch(refereeLength,refereeLength);
+    }
+    
+    else if(refereeLength)
+    {
+        let matchId =  Math.floor(Math.random() * (matchesLength )) + 1;
+        matches_utils.UpdateRefereeToMatch(matchId,refereeLength);
+    }
+    
+    // changes
+    
+} 
 
-exports.CreateReferee = CreateReferee;
+
+exports.CreateRefree = CreateRefree;
