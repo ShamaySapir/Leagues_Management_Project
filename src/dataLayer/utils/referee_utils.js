fce92a38c360c74f916773cbd7eb64fc9a36dad0
ostrovamit <82804444+ostrovamit@users.noreply.github.com>
2021-06-08 20:29:44 +0300

    minor problems

--- src/dataLayer/utils/referee_utils.js
+++ src/dataLayer/utils/referee_utils.js
@@ -2,30 +2,23 @@ const axios = require("axios");
 const DButils = require("../utils/DButils");
 const auth_utils = require("../utils/auth_utils");
 
-
-async function insertRefereeInfo(user) {
-    try {
-        let user_id = await auth_utils.getUserId(user);
-
-        let query = `INSERT INTO dbo.Referees (userId,username) VALUES (${user_id.userId}, '${user.username}');`;
-        DButils.execQuery(
-            `INSERT INTO dbo.Referees (userId,username) VALUES (${user_id.userId}, '${user.username}');`
-        )
-
-        return true;
-
-    } catch (error) {
-        console.log(error);
-    }
-
+async function insertRefereeInfo(id, username) {
+  try {
+    DButils.execQuery(
+      `INSERT INTO dbo.Referees (userId,username) VALUES (${id}, '${username}');`
+    );
+    return true;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getRefereeID(username) {
-    let Referee = await DButils.execQuery(
-        `select refereeId from dbo.Referees WHERE username = ${username};`
-    );
+  let Referee = await DButils.execQuery(
+    `select refereeId from dbo.Referees WHERE username = ${username};`
+  );
 
-    return Referee[0]["refereeId"];
+  return Referee[0]["refereeId"];
 }
 
 exports.insertRefereeInfo = insertRefereeInfo;
