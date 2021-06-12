a873db3f1bee4d25f758916bcc63ece54f379e46
ShamaySapir <shamay.sapir@gmail.com>
2021-06-12 15:51:04 +0300

    execQuery test

--- src/dataLayer/utils/DButils.js
+++ src/dataLayer/utils/DButils.js
@@ -2,66 +2,28 @@ require("dotenv").config();
 const sql = require("mssql");
 
 const config = {
-    user: process.env.tedious_userName,
-    password: process.env.tedious_password,
-    server: process.env.tedious_server,
-    database: process.env.tedious_database,
-    options: {
-        encrypt: true,
-        enableArithAbort: true
-    }
+  user: process.env.tedious_userName,
+  password: process.env.tedious_password,
+  server: process.env.tedious_server,
+  database: process.env.tedious_database,
+  options: {
+    encrypt: true,
+    enableArithAbort: true,
+  },
 };
 
 const pool = new sql.ConnectionPool(config);
 const poolConnect = pool.connect();
 
 async function execQuery(query) {
-    await poolConnect;
-    try {
-        var result = await pool.request().query(query);
-        return result.recordset;
-    } catch (err) {
-        console.error("SQL error", err);
-        throw err;
-    }
-};
-
-async function getTableSize(tableName) {
-    let length = await execQuery(
-        `SELECT COUNT(*) AS LEN FROM ${tableName};`
-    );
-
-    return (length[0])["LEN"];
+  await poolConnect;
+  try {
+    const result = await pool.request().query(query);
+    return result.recordset;
+  } catch (err) {
+    console.error("SQL error", err);
+    throw err;
+  }
 }
 
-exports.getTableSize = getTableSize;
 exports.execQuery = execQuery;
-// process.on("SIGINT", function () {
-//   if (pool) {
-//     pool.close(() => console.log("connection pool closed"));
-//   }
-// });
-
-// poolConnect.then(() => {
-//   console.log("pool closed");
-
-//   return sql.close();
-// });
-
-// exports.execQuery = function (query) {
-//   return new Promise((resolve, reject) => {
-//     sql
-//       .connect(config)
-//       .then((pool) => {
-//         return pool.request().query(query);
-//       })
-//       .then((result) => {
-//         // console.log(result);
-//         sql.close();
-//         resolve(result.recordsets[0]);
-//       })
-//       .catch((err) => {
-//         // ... error checks
-//       });
-//   });
-// };
--- src/dataLayer/utils/DButils.test.js
+++ src/dataLayer/utils/DButils.test.js
@@ -1,67 +1,30 @@
-require("dotenv").config();
-const sql = require("mssql");
+const mssql = require("mssql");
+jest.mock("mssql");
 
-const config = {
-    user: process.env.tedious_userName,
-    password: process.env.tedious_password,
-    server: process.env.tedious_server,
-    database: process.env.tedious_database,
-    options: {
-        encrypt: true,
-        enableArithAbort: true
-    }
-};
+mssql.ConnectionPool = jest.fn();
+const queryResult = jest
+  .fn()
+  .mockImplementation(() => ({ recordset: [{ username: "sapir" }] }));
+mssql.ConnectionPool.mockImplementation(() => ({
+  connect: async () => {},
+  request: () => ({ query: queryResult }),
+}));
+const DButils = require("../utils/DButils");
 
-const pool = new sql.ConnectionPool(config);
-const poolConnect = pool.connect();
+describe("execQuery", () => {
+  test("should return query with the right format", async () => {
+    const query = `SELECT * FROM dbo.Users`;
+    const result = await DButils.execQuery(query);
+    expect(result).toEqual([{ username: "sapir" }]);
+    const queryArgs = queryResult.mock.calls[0][0];
+    expect(queryArgs).toEqual(query);
+  });
+});
 
-async function execQuery(query) {
-    await poolConnect;
-    try {
-        var result = await pool.request().query(query);
-        return result.recordset;
-    } catch (err) {
-        console.error("SQL error", err);
-        throw err;
-    }
-};
-
-async function getTableSize(tableName) {
-    let length = await execQuery(
-        `SELECT COUNT(*) AS LEN FROM ${tableName};`
-    );
-
-    return (length[0])["LEN"];
-}
-
-exports.getTableSize = getTableSize;
-exports.execQuery = execQuery;
-// process.on("SIGINT", function () {
-//   if (pool) {
-//     pool.close(() => console.log("connection pool closed"));
-//   }
-// });
-
-// poolConnect.then(() => {
-//   console.log("pool closed");
-
-//   return sql.close();
-// });
-
-// exports.execQuery = function (query) {
-//   return new Promise((resolve, reject) => {
-//     sql
-//       .connect(config)
-//       .then((pool) => {
-//         return pool.request().query(query);
-//       })
-//       .then((result) => {
-//         // console.log(result);
-//         sql.close();
-//         resolve(result.recordsets[0]);
-//       })
-//       .catch((err) => {
-//         // ... error checks
-//       });
-//   });
+// const mockError = async () => {
+//   throw new Error("some network issue occured");
 // };
+
+// test("should return an error", async () => {
+//   await expect(DButils.execQuery("ran")).rejects.toThrow();
+// });
